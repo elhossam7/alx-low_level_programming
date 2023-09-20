e77a71f3e1a2a7065e7e279f3c14ec0c01aa6335
elhossam7 <elmrabtistyle77@gmail.com>
2023-09-20 06:47:59 -0700

    Noise is a buffer, more effective than cubicles or booth walls

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * string_toupper - change all lowercase to uppercase
+ * @str: pointer
+ *
+ * Return: str
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		str[i] = toupper(str[i]);
+	}
+	return (str);
+}
